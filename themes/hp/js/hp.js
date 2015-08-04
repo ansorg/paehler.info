a0d836d056d26744919128eb9c73fcce4a2521c6
git@ansorg-web.de <rW5EnDb>
2015-08-04 22:48:39 +0200

    simple lightbox implemented

--- themes/hp/js/hp.js
+++ themes/hp/js/hp.js
@@ -43,4 +43,50 @@ hp.slideshow = (function () {
     return me;
 }());
 
+hp.gallery = function (me) {
+
+    me.init = function () {
+        var gallery = document.querySelector('.field-name-field-bilderliste');
+        if (gallery === null) return;
+
+        me.slideBox = document.createElement('div');
+        me.slideBox.id = 'slidebox';
+        me.slideBox.addEventListener('click', me.closeImage, false);
+
+        var container = document.createElement('div');
+        me.slideBox.appendChild(container);
+
+        document.body.appendChild(me.slideBox);
+
+        var images = gallery.querySelectorAll('a');
+        var nrOfOmages = images.length;
+        for (var i = 0; i < nrOfOmages; i++) {
+            images[i].addEventListener('click', me.openImage, false);
+        }
+    };
+
+    me.openImage = function (evt) {
+        if (me.slideBox === null) return;
+        evt.preventDefault();
+        me.slideBox.classList.add('open');
+        me.slideBox.firstChild.innerHTML = '<img src=' + evt.currentTarget.href.replace('/files-x/', '/files/styles/large/public/') + '>';
+
+    };
+
+    me.closeImage = function (evt) {
+        me.slideBox.firstChild.innerHTML = "";
+        me.slideBox.classList.remove('open');
+    };
+
+    return me;
+}(hp);
+
 window.addEventListener('load', hp.slideshow.init);
+window.addEventListener('load', hp.gallery.init);
+
+if ('addEventListener' in document) {
+    document.addEventListener('DOMContentLoaded', function() {
+        FastClick.attach(document.body);
+    }, false);
+}
+
